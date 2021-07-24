1786d5a5fa429754bc5dc291ed9a5fe2c71b231a
Jack Merrill <me@jackmerrill.com>
2021-07-23 23:12:37 -0500

    woo colors change now but it doesn't loop

--- Friendship_Lamp.ino
+++ Friendship_Lamp.ino
@@ -1,15 +1,12 @@
-#include <Base64.h>
-#include <global.h>
-#include <sha1.h>
+#include "defines.h"
+
 #include <Adafruit_NeoPixel.h>
-//#include <WiFiNINA.h>
-//#include <WiFiUdp.h>
 #include "EasyWiFi.h"
 #include <SPI.h>
 #include <WebSockets2_Generic.h>
-#include "defines.h"
 
 using namespace websockets2_generic;
+
 
 /*********** Global Settings  **********/
 EasyWiFi MyEasyWiFi;
@@ -34,29 +31,33 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 #define USE_SERIAL Serial
 bool alreadyConnected = false;
 
-void onEventsCallback(WebsocketsEvent event, String data) 
+const int buttonPin = 11;
+
+void onEventsCallback(WebsocketsEvent event, String data)
 {
   (void) data;
-  
-  if (event == WebsocketsEvent::ConnectionOpened) 
+
+  if (event == WebsocketsEvent::ConnectionOpened)
   {
     Serial.println("Connnection Opened");
-  } 
-  else if (event == WebsocketsEvent::ConnectionClosed) 
+  }
+  else if (event == WebsocketsEvent::ConnectionClosed)
   {
     Serial.println("Connnection Closed");
-  } 
-  else if (event == WebsocketsEvent::GotPing) 
+  }
+  else if (event == WebsocketsEvent::GotPing)
   {
     Serial.println("Got a Ping!");
-  } 
-  else if (event == WebsocketsEvent::GotPong) 
+  }
+  else if (event == WebsocketsEvent::GotPong)
   {
     Serial.println("Got a Pong!");
   }
 }
 
 void setup() {
+
+  pinMode(buttonPin, INPUT);
 
   pixels.begin();
 
@@ -80,6 +81,22 @@ void setup() {
   MyEasyWiFi.seed(0);
 
   while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("* Not Connected, starting EasyWiFi");
+    for (int j = 0; j < 4; j++) {
+      for (int i = 0; i < NUMPIXELS; i++) {
+        pixels.clear();
+        pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+
+        pixels.show();   // Send the updated pixel colors to the hardware.
+
+        delay(DELAYVAL); // Pause before next pass through loop
+      }
+    }
+    pixels.clear();
+    for (int i = 0; i < NUMPIXELS; i++) {
+      pixels.setPixelColor(i, pixels.Color(155, 155, 0));
+    }
+    pixels.show();
     if (WiFi.status() == WL_CONNECTED) {
       break;
     } else {
@@ -87,10 +104,21 @@ void setup() {
     }
   }
 
-  client.onMessage([&](WebsocketsMessage message) 
+  pixels.clear();
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+  }
+  pixels.show();
+
+  delay(1500);
+
+  client.onMessage([&](WebsocketsMessage message)
   {
-    Serial.print("Got Message: ");
-    Serial.println(message.data());
+    pixels.clear();
+    for (int i = 0; i < NUMPIXELS; i++) {
+      pixels.setPixelColor(i, message.data().toInt());
+    }
+    pixels.show();
   });
 
   // run callback when events are occuring
@@ -101,60 +129,74 @@ void setup() {
 
 void sendMessage(void)
 {
-// try to connect to Websockets server
+  // try to connect to Websockets server
   bool connected = client.connect(websockets_server_host, websockets_server_port, "/");
-  
-  if (connected) 
+
+  if (connected)
   {
     Serial.println("Connected!");
 
-    String WS_msg = String("Hello to Server from ") + BOARD_NAME;
-    client.send(WS_msg);
-  } 
-  else 
+    client.send("getColor");
+  }
+  else
   {
     Serial.println("Not Connected!");
   }
 }
 
+int buttonPushCounter = 0;
+int buttonState = 0;
+int lastButtonState = 0;
+
+int colors[] = { 16711680, 16746240, 16765696, 14614282, 10616586, 720793, 716799, 1342965, 5769983, 12454655 };
+
+void updateColor(int color) {
+  pixels.clear();
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, colors[color]);
+  }
+  pixels.show();
+
+  Serial.println("color");
+
+  client.send("setColor " + colors[color]);
+}
+
 void loop() {
   if (WiFi.status() == WL_CONNECTED)
   {
-    printWiFiStatus();
     if (client.available()) {
-      client.poll();
+      Serial.println("available");
+      buttonState = digitalRead(buttonPin);
+
+      // compare the buttonState to its previous state
+      if (buttonState != lastButtonState) {
+        // if the state has changed, increment the counter
+        if (buttonState == HIGH) {
+          // if the current state is HIGH then the button went from off to on:
+          buttonPushCounter++;
+          Serial.println("on");
+          Serial.print("number of button pushes: ");
+          Serial.println(buttonPushCounter);
+          updateColor(buttonPushCounter - 1);
+        } else {
+          // if the current state is LOW then the button went from on to off:
+          Serial.println("off");
+        }
+        // Delay a little bit to avoid bouncing
+        delay(50);
+      }
+      // save the current state as the last state, for next time through the loop
+      lastButtonState = buttonState;
+      if (buttonPushCounter == 10) {
+        buttonPushCounter = 0;
+      }
+//      client.poll();
     }
-    pixels.clear();
-    for (int i = 0; i < NUMPIXELS; i++) { // For each pixel...
-      // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-      // Here we're using a moderately bright green color:
-      pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-    }
-    pixels.show();
   }
   else
   {
-    Serial.println("* Not Connected, starting EasyWiFi");
-    for (int j = 0; j < 4; j++) {
-      for (int i = 0; i < NUMPIXELS; i++) { // For each pixel...
-        pixels.clear(); // Set all pixel colors to 'off'
-
-        // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-        // Here we're using a moderately bright green color:
-        pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-
-        pixels.show();   // Send the updated pixel colors to the hardware.
-
-        delay(DELAYVAL); // Pause before next pass through loop
-      }
-    }
-    pixels.clear();
-    for (int i = 0; i < NUMPIXELS; i++) { // For each pixel...
-      // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
-      // Here we're using a moderately bright green color:
-      pixels.setPixelColor(i, pixels.Color(155, 155, 0));
-    }
-    pixels.show();
+    Serial.println("disconnected");
   }
 }
 
